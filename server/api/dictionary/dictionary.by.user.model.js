d358e0396fe1a6574e2623b8837b1b4158605e94
Pavel R <rabeckijps@gmail.com>
2016-03-05 20:42:55 +0100

    fix jshint errors

--- server/api/dictionary/dictionary.by.user.model.js
+++ server/api/dictionary/dictionary.by.user.model.js
@@ -3,9 +3,8 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var DictionaryByUserSchema = new mongoose.Schema({
-  _id: ObjectId,
   dictionarys: [{
-  	dictionary_id: ObjectId,
+  	dictionary_id: mongoose.ObjectId,
   	name : String
   }]
 });
