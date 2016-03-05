cb322bfa7a1a3dd5f363d2b1078712bbf6540551
Pavel R <rabeckijps@gmail.com>
2016-03-05 12:39:52 +0100

    seed production db

--- server/api/dictionary/dictionary.model.js
+++ server/api/dictionary/dictionary.model.js
@@ -4,8 +4,12 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var DictionarySchema = new mongoose.Schema({
   name: String,
-  info: String,
-  active: Boolean
+  cards: [{
+  	name : String,
+  	translation : String,
+  	soundUrl : String,
+  	examples : String
+  }]
 });
 
 export default mongoose.model('Dictionary', DictionarySchema);
