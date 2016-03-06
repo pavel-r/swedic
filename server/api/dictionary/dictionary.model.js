73645897b5f2c3d6f6e751e576d5f6c6d9ca81aa
Pavel R <rabeckijps@gmail.com>
2016-03-06 12:35:43 +0100

    change dictionary controller

--- server/api/dictionary/dictionary.model.js
+++ server/api/dictionary/dictionary.model.js
@@ -4,6 +4,7 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var DictionarySchema = new mongoose.Schema({
   name: String,
+  user_id: String,
   cards: [{
   	name : String,
   	translation : String,
