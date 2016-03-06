dd69e656b65e48db1a6af4b1c747b2223874913f
Pavel R <rabeckijps@gmail.com>
2016-03-06 17:00:33 +0100

    update client

--- client/app/edit/edit.controller.js
+++ client/app/edit/edit.controller.js
@@ -3,14 +3,14 @@
 angular.module('swedicApp')
   .controller('EditCtrl', function ($scope, $stateParams, $http) {
   	this.$http = $http;
-  	this.editing = false;
+  	$scope.editing = false;
 
   	this.$http.get('/api/dictionarys/' + $stateParams.id).then(response => {
-  		this.dictionary = response.data;
+  		$scope.dictionary = response.data;
   	});
 
   	this.addNewCard = function(){
-  		this.editing = true;
+  		$scope.editing = true;
   	};
 
     $scope.message = 'Hello';
