5e93635dff2c44f69466d38cae4fb3ea28f9cae0
Pavel R <rabeckijps@gmail.com>
2016-03-06 16:28:01 +0100

    update client

--- client/app/edit/edit.controller.js
+++ client/app/edit/edit.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('swedicApp')
-  .controller('EditCtrl', function ($scope, $stateParams) {
+  .controller('EditCtrl', function ($scope, $stateParams, $http) {
+  	this.$http = $http;
+
+  	this.$http.get('/api/dictionarys/' + $stateParams.id).then(response => {
+  		this.dictionary = response.data;
+  	});
+  	
     $scope.message = 'Hello';
   });
