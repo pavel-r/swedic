cb8028381a3921aa2006a63b076b4e7a21d126bc
Pavel R <rabeckijps@gmail.com>
2016-03-27 09:22:10 +0000

    add caching for dictionaries

--- client/components/dictionarys/dictionarys.service.js
+++ client/components/dictionarys/dictionarys.service.js
@@ -4,16 +4,38 @@ angular.module('swedicApp')
   .service('dictionarys', function ($q, $http) {
     var self = this;
 
-    // var dictionarysCache;
+    var dictionarysCache;
+
+    function invalidateCache(){
+      dictionarysCache = null;
+    }
+    
+    function setDictionarysCache(data){
+      dictionarysCache = data;
+    }
 
     this.getDictionarys = function(){
-      return $http.get('/api/dictionarys').then(response => {
-        // dictionarysCache = response.data;
-        return response.data;
-      });
+      if(dictionarysCache){
+        return $q.when(dictionarysCache);
+      } else {
+        return $http.get('/api/dictionarys').then(response => {
+          setDictionarysCache(response.data);
+          return response.data;
+        });
+      }
     };
 
     this.getDictionaryById = function(id){
+      if(dictionarysCache){
+        var dictionary = dictionarysCache.filter((d) => { 
+          return d._id === id;
+        })[0];
+        if(dictionary){
+          return $q.when(dictionary);
+        } else {
+          invalidateCache();
+        }
+      }
       return $http.get('/api/dictionarys/' + id).then(response => {
         return response.data;
       });
@@ -21,23 +43,19 @@ angular.module('swedicApp')
 
     this.addDictionary = function(dictionary) {
       var self = this;
+      invalidateCache();
       return $http.post('/api/dictionarys', dictionary)
         .then(self.getDictionarys);
     };
-    // this.getDictionarys = function(){
-    //   if(dictionarysCache){
-    //     return $q.when(dictionarysCache);
-    //   } else {
-    //     return self.loadDictionarys();
-    //   }
-    // };
     
     this.deleteDictionary = function(dictionary){
+      invalidateCache();
       return $http.delete('/api/dictionarys/' + dictionary._id) // jshint ignore:line
         .then(self.getDictionarys);
     };
     
     this.addCardToDictionary = function(dictionary, newCard){
+      invalidateCache();
       // dictionary.cards.push(newCard);
       return $http.post('/api/dictionarys/' + dictionary._id + '/cards', newCard).then(response => {
         	return response.data;
@@ -45,6 +63,7 @@ angular.module('swedicApp')
     };
     
     this.deleteCardFromDictionary = function(dictionary, card) {
+      invalidateCache();
     //   dictionary.cards = dictionary.cards.filter( c => {
     // 		return c !== card;
     // 	});
@@ -54,6 +73,7 @@ angular.module('swedicApp')
     };
     
     this.updateCardInDictionary = function(dictionary, card) {
+      invalidateCache();
     //   dictionary.cards = dictionary.cards.filter( c => {
     // 		return c !== card;
     // 	});
--- client/components/dictionarys/dictionarys.service.spec.js
+++ client/components/dictionarys/dictionarys.service.spec.js
@@ -4,15 +4,61 @@ describe('Service: dictionarys', function () {
 
   // load the service's module
   beforeEach(module('swedicApp'));
+  beforeEach(module('stateMock'));
 
+  var dummyDictionary = {
+    _id : '123',
+    name: 'dictionary',
+    cards : []
+  };
+  
   // instantiate service
-  var dictionarys;
-  beforeEach(inject(function (_dictionarys_) {
+  var dictionarys, 
+      $httpBackend;
+  beforeEach(inject(function (_dictionarys_, _$httpBackend_) {
     dictionarys = _dictionarys_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  it('should do something', function () {
-    expect(!!dictionarys).toBe(true);
+  it('should get dictionarys from backend', function () {
+    $httpBackend.expectGET('/api/dictionarys').respond([dummyDictionary]);
+    dictionarys.getDictionarys().then((d) => {
+      expect(d).toEqual([dummyDictionary]);
+    });
+    $httpBackend.flush();
+  });
+  
+  it('should get dictionarys from cache for subsequent request', function () {
+    $httpBackend.expectGET('/api/dictionarys').respond([dummyDictionary]);
+    dictionarys.getDictionarys()
+     .then(() => {
+       return dictionarys.getDictionarys();
+     })
+     .then((d) => {
+       expect(d).toEqual([dummyDictionary]);
+     });
+    $httpBackend.flush();
+  });
+  
+  it('should get dictaionary by id from backend', function(){
+    $httpBackend.expectGET('/api/dictionarys/123').respond(dummyDictionary);
+    dictionarys.getDictionaryById('123')
+      .then((d) => {
+        expect(d).toEqual(dummyDictionary);
+      });
+    $httpBackend.flush();
   });
 
+  it('should get dictaionary by id from cache if exists in cache', function(){
+    $httpBackend.expectGET('/api/dictionarys').respond([dummyDictionary]);
+    dictionarys.getDictionarys()
+      .then(() => {
+        return dictionarys.getDictionaryById('123');
+      })
+      .then((d) => {
+        expect(d).toEqual(dummyDictionary);
+      });
+    $httpBackend.flush();
+  });
+  
 });
