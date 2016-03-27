9ddf06e2f7056142266daf1b29129e68cae53308
Pavel R <rabeckijps@gmail.com>
2016-03-27 18:39:19 +0000

    lambda fix in editCard controller

--- client/app/editcard/editcard.controller.js
+++ client/app/editcard/editcard.controller.js
@@ -13,7 +13,7 @@ class EditCardController {
 
     dictionarys.getDictionaryById($stateParams.id).then(dictionary => {
       self.dictionary = dictionary;
-      self.card = dictionary.cards.find(c => {
+      self.card = dictionary.cards.find((c) => {
         return c._id === self.$stateParams.cardId;
       });
     });
